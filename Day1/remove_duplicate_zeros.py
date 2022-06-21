0de6019139c6da1bc81c87c5d567743119d3c776
Sneha Jha <snehajha08112000@gmail.com>
2022-06-21 11:37:46 +0530

    Create remove_duplicate_zeros.py

---
+++ Day1/remove_duplicate_zeros.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        x=1
+        for i in range(len(nums)-1):
+            if(nums[i]!=nums[i+1]):
+                nums[x]=nums[i+1]
+                x+=1
+        
+        return x;
+      
+      ### in this question we are replacing each position by a different number and returning the no.of different numbers
+      
+      
