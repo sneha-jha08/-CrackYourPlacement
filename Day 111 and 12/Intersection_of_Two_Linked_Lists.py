bec7c3601fab1e1c4d6849a6f254e36e2b957ac7
Sneha Jha <snehajha08112000@gmail.com>
2022-07-16 00:53:56 +0530

    Create Intersection_of_Two_Linked_Lists.py

---
+++ Day 111 and 12/Intersection_of_Two_Linked_Lists.py
@@ -0,0 +1,15 @@
+class Solution:
+    def getIntersectionNode(self, headA, headB):
+        if headA and headB:
+            A, B = headA, headB
+            while A!=B:
+                #A = A.next if A else headB
+                #B = B.next if B else headA
+                
+                if A: A=A.next
+                else:A=headB
+                    
+                if B:B=B.next
+                else:B=headA
+            return A
+          
